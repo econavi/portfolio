fd142ce7688d84e603f3ddf4822d4e9a1d6a3292
Vladimir Kochenov <kochenov.vl@gmail.com>
2018-04-18 10:19:59 +0300

    добавил проект Arendastart

--- app/blocks/main-menu/main-menu.js
+++ app/blocks/main-menu/main-menu.js
@@ -1,8 +1,9 @@
-var elem = document.querySelectorAll('.main-menu__link');
+var elems = document.querySelectorAll('.main-menu__link');
 
-for(var i = 0; i < elem.length; i++) {
-	if(elem[i].getAttribute('href') == window.location.pathname) {
-		elem[i].classList.add('main-menu__link_active');
-		elem[i].removeAttribute('href');
+for(var i = 0; i < elems.length; i++) {
+	if(elems[i].getAttribute('href') == window.location.pathname) {
+		elems[i].classList.add('main-menu__link_active');
+		elems[i].removeAttribute('href');
+		elems[i].style.cursor = 'default';
 	}
 }
