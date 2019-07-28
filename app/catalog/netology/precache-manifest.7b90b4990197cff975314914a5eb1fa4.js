545ef236902e3ad5d3cd9ffeb932cb726a6dd0a8
Vladimir Kochenov <kochenov.vl@gmail.com>
2019-07-28 19:44:18 +0300

    Add netology/person-list-app

---
+++ app/catalog/netology/precache-manifest.7b90b4990197cff975314914a5eb1fa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee1f9ade6b80bdd5ccb33f3bfbfa3d50",
+    "url": "/catalog/netology/person-list-app/index.html"
+  },
+  {
+    "revision": "33abcb4711d67574be34",
+    "url": "/catalog/netology/person-list-app/static/css/main.39566a8b.chunk.css"
+  },
+  {
+    "revision": "97833c2c9d630fe30281",
+    "url": "/catalog/netology/person-list-app/static/js/2.9369287a.chunk.js"
+  },
+  {
+    "revision": "33abcb4711d67574be34",
+    "url": "/catalog/netology/person-list-app/static/js/main.9c819669.chunk.js"
+  },
+  {
+    "revision": "90db1074a0c0339ee398",
+    "url": "/catalog/netology/person-list-app/static/js/runtime~main.7a6dbab7.js"
+  },
+  {
+    "revision": "83c80085f0f5a86bebb5d1f8a60ae24e",
+    "url": "/catalog/netology/person-list-app/static/media/netology-group-logo.83c80085.svg"
+  }
+]);
